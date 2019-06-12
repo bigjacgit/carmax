442505a22efdfaf0eb46370da16c59ed156a50ad
bigjacgit <jacob.b.dba@gmail.com>
2019-06-12 12:39:10 -0400

    Update GitSetup.sh

--- GitSetup.sh
+++ GitSetup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bsh
 
-mkdir -p ${1}/carmax
-cd ${1}/carmax
+mkdir -p ${1}/JobUpdate
+cd ${1}/JobUpdate
 pwd
 git init
 git config --global user.email "jacob.b.dba@gmail.com"
 git config --global user.name "jacob"
-git remote add carmaxgit https://github.com/bigjacgit/carmax.git
+git remote add JobUpdateGit https://github.com/bigjacgit/JobUpdate.git
 ls 
 pwd
