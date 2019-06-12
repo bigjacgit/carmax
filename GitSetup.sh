6d3c021ddb7964d3b4cfb978302f9e7fcb3492d6
bigjacgit <jacob.b.dba@gmail.com>
2019-06-12 12:17:21 -0400

    Update GitSetup.sh

--- GitSetup.sh
+++ GitSetup.sh
@@ -6,5 +6,6 @@ pwd
 git init
 git config --global user.email "jacob.b.dba@gmail.com"
 git config --global user.name "jacob"
+git remote add carmaxgit https://github.com/bigjacgit/carmax.git
 ls 
 pwd
